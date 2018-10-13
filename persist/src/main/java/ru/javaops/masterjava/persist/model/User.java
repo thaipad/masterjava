da22faf7bd8ffa7fe65c5f8486efb85e35b3f56a
thaipad <me@thaipad.pro>
2018-10-13 20:32:52 +0700

    Revert "5 7 lombok"
    This reverts commit 145b090

--- persist/src/main/java/ru/javaops/masterjava/persist/model/User.java
+++ persist/src/main/java/ru/javaops/masterjava/persist/model/User.java
@@ -2,23 +2,19 @@ package ru.javaops.masterjava.persist.model;
 
 import com.bertoncelj.jdbi.entitymapper.Column;
 import lombok.*;
-import ru.javaops.masterjava.persist.model.type.UserFlag;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-@ToString(callSuper = true)
+@NoArgsConstructor
 public class User extends BaseEntity {
     @Column("full_name")
     private @NonNull String fullName;
     private @NonNull String email;
     private @NonNull UserFlag flag;
-    @Column("city_ref")
-    private @NonNull String cityRef;
 
-    public User(Integer id, String fullName, String email, UserFlag flag, String cityRef) {
-        this(fullName, email, flag, cityRef);
+    public User(Integer id, String fullName, String email, UserFlag flag) {
+        this(fullName, email, flag);
         this.id=id;
     }
 }
--- web/upload/src/main/java/ru/javaops/masterjava/upload/UploadServlet.java
+++ web/upload/src/main/java/ru/javaops/masterjava/upload/UploadServlet.java
@@ -23,7 +23,7 @@ import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
 public class UploadServlet extends HttpServlet {
     private static final int CHUNK_SIZE = 2000;
 
-    private final PayloadProcessor payloadProcessor = new PayloadProcessor();
+    private final UserProcessor userProcessor = new UserProcessor();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -42,7 +42,7 @@ public class UploadServlet extends HttpServlet {
             } else {
                 Part filePart = req.getPart("fileToUpload");
                 try (InputStream is = filePart.getInputStream()) {
-                    List<PayloadProcessor.FailedEmails> failed = payloadProcessor.process(is, chunkSize);
+                    List<UserProcessor.FailedEmails> failed = userProcessor.process(is, chunkSize);
                     log.info("Failed users: " + failed);
                     final WebContext webContext =
                             new WebContext(req, resp, req.getServletContext(), req.getLocale(),
